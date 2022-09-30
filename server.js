1799e043dd58f7011d2ce262d36fe17d4c91a7b7
valerievozza <valerievozza@gmail.com>
2022-09-30 01:03:36 -0700

    add income routes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const expenseRoutes = require("./routes/expense");
+const incomeRoutes = require("./routes/income");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -56,6 +57,7 @@ app.use(flash());
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/expense", expenseRoutes);
+app.use("/income", incomeRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
